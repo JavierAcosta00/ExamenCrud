9b857dc5aaab490e038426c73b0115ffb5ea9a1a
dsad <dasda>
2022-11-14 09:09:22 -0700

    Initial Commit

---
+++ Examen/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Examen
+//
+//  Created by CCDM28 on 14/11/22.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Examen/ExamenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenApp.swift
+//  Examen
+//
+//  Created by CCDM28 on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ExamenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
